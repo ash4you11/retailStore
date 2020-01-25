25b8e9393d498ecbdf5a154bdff6406237e3c309
ashishkumar09 <ashishkumar09@ashishkumar0.Nagarro.local>
2020-01-25 14:51:09 +0530

    putting extra comment

--- src/main/java/com/retails/service/impl/DiscountServiceImpl.java
+++ src/main/java/com/retails/service/impl/DiscountServiceImpl.java
@@ -13,13 +13,16 @@ public class DiscountServiceImpl implements DiscountService {
 	public double calculateDiscount(double totalAmount, User user) {
 		
 		if(user.getType()==UserType.AFFILIATE) {
+			//10 percent discount for affiliate
 			return totalAmount-(totalAmount*(.1));
 		}
 		else if(user.getType()==UserType.EMPLOYEE) {
+			//10 percent discount for employee
 			return totalAmount-(totalAmount*(.3));
 		}
 		
 		else if(user.getType()==UserType.SIMPLE && user.isUserTwoYearsAssociation()) {
+			//5 percent discount for user with 2 years association
 			return totalAmount-(totalAmount*(.05));
 		}
 		//there will be no discount 
